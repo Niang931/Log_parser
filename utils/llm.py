a6918267f83f7078df4dd17a0eb4118d58974495
Niang931 <kytnlmw1226@gmail.com>
2026-04-25 16:39:10 +0800

    add abstract llm client layer

--- utils/llm.py
+++ utils/llm.py
@@ -1,15 +1,13 @@
 import json
 import time
-from google import genai
-from google.genai.errors import ServerError, ClientError, APIError
 import random
-from google.genai import types
 from threading import *
-import requests
 import logging
 from token_bucket import TokenBucket
 import logfire
 import os
+from abc import ABC, abstractmethod
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -19,42 +17,37 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
-os.environ['OTEL_INSTRUMENTATION_GENAI_CAPTURE_MESSAGE_CONTENT'] = 'true'
-logfire.configure()
-logfire.instrument_google_genai()
 
-class LLMParser:
-    SAFE_TO_RETRY = [429, 500, 503]
+# os.environ['OTEL_INSTRUMENTATION_GENAI_CAPTURE_MESSAGE_CONTENT'] = 'true'
+# logfire.configure()
+# logfire.instrument_google_genai()
+# logfire.instrument_openai()
+
+class LLMParser(ABC):
+    SAFE_TO_RETRY = [429, 500, 503, 502]
     MAX_RETRY = 5
     BASE_DELAY = 1
     MAX_PARALLEL_CALL = 5
     TIMEOUT_THRESHOLD = 100000
 
     def __init__(self):
-        self.client = genai.Client(api_key="AIzaSyA2Y-rN2mcjp1JY2jnPpwRn01edd5rYDrU",
-                                   http_options=types.HttpOptions(timeout=self.TIMEOUT_THRESHOLD))
         self.count = 0
         self.obj = Semaphore(self.MAX_PARALLEL_CALL)
         self.token_bucket = TokenBucket()
 
-    def call_llm(self, prompt):
+    def wrapper(self, prompt):
         self.count += 1
+        self.token_bucket.consume()
+        self.obj.acquire()
         try:
-            self.token_bucket.consume()
-            self.obj.acquire()
-            response = self.client.models.generate_content(
-                model="gemini-2.5-flash-lite",
-                contents=prompt
-            )
-
+            response = self.generate(prompt)
             self.obj.release()
             logger.info(f'{self.obj}')
             logger.info(f'Remaining tokens:{self.token_bucket.tokens}')
             logger.info('API call successful')
-            return response.text
+            return response
 
-
-        except (ServerError, ClientError, APIError) as e:
+        except Exception as e:
             status_code, error_message = self.extract_message(e)
             logger.error(f'{error_message}')
             if status_code in self.SAFE_TO_RETRY:
@@ -64,10 +57,13 @@ class LLMParser:
                     delay_time = self.BASE_DELAY * (2 ** self.count) + jitter
                     time.sleep(delay_time)
                     logger.info('LLM Retry')
-                    self.call_llm(prompt)
+                    self.wrapper(prompt)
                 else:
                     logger.info('Out of retries')
 
+    @abstractmethod
+    def generate(self, prompt):
+        pass
 
     def load_template(self, text):
         if text:
@@ -75,30 +71,16 @@ class LLMParser:
                 print(text, file=in_file)
             logger.info('Load templates')
 
+    @abstractmethod
     def extract_message(self, e):
-        status_code = e.code
-        print(f"HTTP error code: {status_code}")
-        error_message =e.status
-        print(f"Error message: {error_message}")
-        return status_code, error_message
-
-
+        pass
 
 if __name__ == '__main__':
     parser = LLMParser()
-    prompt = ("Given this log file: Create the template for it by replacing all the dynamic variables with <*> "
-              "while keeping the fixed values"
-              "Answer ONLY the template with no extra word ")
+    prompt = "Create the template for log by replacing all the dynamic variables with <*>"
     with open('sample.log', 'r') as f:
         log = f.read()
-    response= parser.call_llm(f'{prompt} {log}')
-    parser.load_template(response)
-    # response1 = parser.call_llm(f'{prompt} {log}')
-    # parser.load_template(response1)
-    # response2 = parser.call_llm(f'{prompt} {log}')
-    # parser.load_template(response2)
-    # response3 = parser.call_llm(f'{prompt} {log}')
-    # parser.load_template(response3)
-    # response4 = parser.call_llm(f'{prompt} {log}')
-    # parser.load_template(response4)
+    prompt = f'{prompt} {log}'.strip()
+    print(prompt)
+
 
